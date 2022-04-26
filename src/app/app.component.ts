2758b4b9a28d601b1771eb184b02bf31e61a9228
Emeline SANCHEZ <emelinemsanchez@gmail.com>
2022-04-26 16:56:05 +0200

    Update 26/04

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Sudoku-Angular';
+  maVariable = [
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    ''
+];
+res = [
+  5,
+  9,
+  1,
+  4,
+  8,
+  2,
+  6,
+  3,
+  7
+]
+  verifNombre(nbr : any, id : number){
+    if(nbr == this.res[id]){
+      console.log('Correct !')
+    }
+    else{
+      console.log('Incorrect !')
+    }
+  }
 }
