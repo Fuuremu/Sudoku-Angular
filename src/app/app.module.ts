e3ff8343809e75fb44859cf26cd4056606646173
ilham <arbouchilham1@gmail.com>
2022-05-05 10:31:26 +0200

    Le timer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
+import { TimerComponent } from './timer/timer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent
+    UserComponent,
+    TimerComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/timer/timer.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { timer } from 'rxjs';
+
+@Component({
+  selector: 'app-timer',
+  templateUrl: './timer.component.html',
+  styleUrls: ['./timer.component.css']
+})
+export class TimerComponent {
+  subscribeTimer: any;
+  timeLeft: number = 60;
+  constructor() { }
+
+  startTimer() {
+    const source = timer(1000, 2000);
+    const abc = source.subscribe(val => {
+      console.log(val, '-');
+      this.subscribeTimer = this.timeLeft - val;
+    });
+  }
+  
+  // <p (click)="observableTimer()">Start Observable timer</p> {{subscribeTimer}}
+}
